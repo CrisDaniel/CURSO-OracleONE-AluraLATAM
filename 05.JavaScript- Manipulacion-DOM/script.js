a671b4ed1e35b9013a3341a5d0c516c697cdf29a
Cristian <daniel981311@gmail.com>
2023-06-30 16:47:22 -0500

    script contiene la creacion de tareas y acceso a los modulos a traves de import

--- 05.JavaScript- Manipulacion-DOM/script.js
+++ 05.JavaScript- Manipulacion-DOM/script.js
@@ -1,23 +1,37 @@
+import checkComplete from "./components/checkComplete.js"
+import deleteIcon from "./components/deleteIcon.js"
+
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (event) => {
   const input = document.querySelector("[data-form-input]");
-  const list = document.querySelector("[data-list]");
   const value = input.value;
+  const list = document.querySelector("[data-list]");
   event.preventDefault(); //Para evitar que el btn envie la informacion automaticamente por su tipo "submit" y actualize la consola
   const task = document.createElement('li');
   task.classList.add('card');
 
   input.value = "";
-  const content = `
-  <div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>`;
-  task.innerHTML = content;
+  const taskContent = document.createElement("div");
+  const titleTask = document.createElement("span");
+  titleTask.classList.add('task');
+  titleTask.innerText = value;
+
+  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(titleTask);
+  task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
   list.appendChild(task);
-  console.log(content);
+
+  /*
+  <li class="card">   Creamos este elemento
+      <div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">Hacer curso de DOM</span>
+      </div>
+      <i class="fas fa-trash-alt trashIcon icon"></i>
+  </li> 
+  */
 };
 
 btn.addEventListener("click", createTask);

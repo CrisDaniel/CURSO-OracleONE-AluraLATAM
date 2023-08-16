d3efc258011d3e9d414f799052627fb908db4633
Cristian <daniel981311@gmail.com>
2023-08-16 17:07:45 -0500

    Proyecto en REACT

---
+++ 08-React/app-react-org/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header/header.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hola mundo con React
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprende React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 08-React/app-react-org/src/components/header/header.jsx
@@ -0,0 +1,9 @@
+import './header.css';
+
+function Header(){
+    return <header className="header">
+        <img src="/img/HeaderHeader_total.png" alt="Cabecera Org"/>;
+    </header>;
+}
+
+export default Header;

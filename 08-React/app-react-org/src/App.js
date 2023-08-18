8767b3ce122beb133304d900b66463e3e6255f08
Cristian <daniel981311@gmail.com>
2023-08-18 18:17:12 -0500

    Update

--- 08-React/app-react-org/src/App.js
+++ 08-React/app-react-org/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header.jsx';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo con React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
     </div>
   );
 }

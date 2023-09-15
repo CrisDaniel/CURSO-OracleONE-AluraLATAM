ac8cab2067232a9f01784ddda959d7fed1b80eb5
Cristian <daniel981311@gmail.com>
2023-09-15 00:35:47 -0500

    Page 404

--- 09-React-Router-SPA/src/App.js
+++ 09-React-Router-SPA/src/App.js
@@ -1,11 +1,21 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Page404 from './pages/Page404';
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <>
 
-    </>
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Sobre' element={<Sobre/>}/>
+        <Route path='*' element={<Page404/>}/>
+      </Routes>
+    </Router>
   );
 }
 

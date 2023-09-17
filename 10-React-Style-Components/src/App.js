d500515df2587ea101a1a036a87ae61ae4551c73
Cristian <daniel981311@gmail.com>
2023-09-17 06:04:13 -0500

    CreateGlobalStyle - style global

--- 10-React-Style-Components/src/App.js
+++ 10-React-Style-Components/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Container from "./Components/Container";
 import Header from "./Components/Header";
+import GlobalStyle from "./GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle/>
       <Header />
       <Container />
     </>

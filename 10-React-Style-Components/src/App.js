8a73d71c84b44de00bd52da8dd7ab7abeb2e6d8d
Cristian <daniel981311@gmail.com>
2023-09-16 20:53:32 -0500

    Proyecto Smart Bank

---
+++ 10-React-Style-Components/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Container from "./Components/Container";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container />
+    </>
+  );
+}
+
+export default App;

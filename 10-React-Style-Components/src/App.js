5d7f5ce4d7388790153b4a306ad949b9e66b4ce1
Cristian <daniel981311@gmail.com>
2023-09-21 18:09:19 -0500

    Tema oscuro

--- 10-React-Style-Components/src/App.js
+++ 10-React-Style-Components/src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Container from "./Components/Container";
 import Header from "./Components/Header";
 import GlobalStyle from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaOscuro } from "./Components/UI/temas";
+import {BtnTemaStyle} from "./Components/UI";
+import BtnTema from "./Components/BtnTema";
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const toggleTema = () => {
+    setTema((tema) => !tema);
+  };
+
   return (
-    <>
+    <ThemeProvider theme={ tema ? temaClaro : temaOscuro}>
       <GlobalStyle/>
+      <BtnTemaStyle onClick={toggleTema}>
+        <BtnTema  tema={tema}/>
+      </BtnTemaStyle>
       <Header />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 

bb077cb411da07306f159840c8dc20985b2e69ab
Cristian <daniel981311@gmail.com>
2023-09-28 08:23:09 -0500

    Validacion de formularios y hooks

--- 11-react-formulario/src/App.js
+++ 11-react-formulario/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import Container from '@mui/material/Container';
 import FormSingUp from './components/FormSingUp';
+import Typography from '@mui/material/Typography';
 
 function App() {
+
+  const setValue = (values) => {
+    console.log('Valores: ', values);
+  };
+
   return (
-    <>
-      <h1>Formulario Registro</h1>
+    <Container component="section" maxWidth="sm">
+      <Typography variant='h3' align='center' component="section">Formulario Registro</Typography>
       <hr/>
-      <FormSingUp/>
-    </>
+      <FormSingUp setValue={setValue}/>
+    </Container>
   );
 }
 
--- 11-react-formulario/src/components/FormSingUp.jsx
+++ 11-react-formulario/src/components/FormSingUp.jsx
@@ -1,7 +1,55 @@
-function FormSingUp(){
-    return <form>
-        <input type="text"/>
-        <button>Registrarse</button>
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+
+function FormSingUp(props){
+
+    const {setValue} = props;
+
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [prom, setProm] = useState(true);
+    const [nov, setNov] = useState(false);
+
+    const [errors, setErrores] = useState({
+        name: {
+            error: false,
+            message: "Deben ser almenos 3 caracteres",
+        }
+    });
+
+    const validarNombre = (nombre) => {
+        if(nombre.length >=3){
+            return {name: {error: false, message: ""}}
+        }else{
+            return {name: {error: true, message: "Deben ser almenos 3 caracteres"}}
+        }
+    }
+
+    return <form onSubmit={(evento) => {
+                evento.preventDefault();
+                setValue({name, lastName, email, prom, nov});
+            }}>
+        <TextField id="Nombres" label="Nombres" variant="outlined" fullWidth margin='normal' onChange={(evento) =>  setName(evento.target.value)} value={name}
+            error={errors.name.error}
+            helperText={errors.name.error ? errors.name.message : ""}
+            onBlur={(e) => {
+                setErrores(validarNombre(e.target.value))
+            }}
+        />
+        <TextField id="Apellidos" label="Apellidos" variant="outlined" fullWidth margin='dense' onChange={(evento) =>  setLastName(evento.target.value)} value={lastName}/>
+        <TextField id="Correo electronico" label="Correo electronico" variant="outlined" fullWidth margin='dense' onChange={(evento) =>  setEmail(evento.target.value)} value={email}/>
+        <FormGroup>
+            <FormControlLabel control={<Switch checked={prom}/>} label="Promociones" onChange={(evento) =>  setProm(evento.target.checked)}/>
+            <FormControlLabel control={<Switch checked={nov}/>} label="Novedades" onChange={(evento) =>  setNov(evento.target.checked)}/>
+        </FormGroup>
+
+        <Button variant="contained" endIcon={<SendIcon/>} type='submit'>Registrarse</Button>
     </form>
 }
 

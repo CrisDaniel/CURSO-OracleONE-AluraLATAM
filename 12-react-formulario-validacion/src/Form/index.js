d23fb68c45f135c84f64a826304c53640536f400
Cristian <daniel981311@gmail.com>
2023-09-29 09:03:56 -0500

    finalizacion del ProgramaOracle con EXITO :-)

--- 12-react-formulario-validacion/src/Form/index.js
+++ 12-react-formulario-validacion/src/Form/index.js
@@ -8,7 +8,7 @@ import Complete from "./Complete";
 import Stepper from "../Stepper";
 
 const Form = () => {
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(0);
 
   const updateStep = (step) => {
     setStep(step);
